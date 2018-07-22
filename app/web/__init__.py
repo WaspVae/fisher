03e1ae1ab8e118621974faa1327f4e2b1ac9a337
你的东哥哥 <waspvae@gmail.com>
2018-07-22 14:11:12 +0800

    Update __init__.py

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -4,9 +4,14 @@ from flask import render_template
 web = Blueprint('web', __name__)
 
 
-@web.app_errorhandler(404)
-def not_found(e):
-    return render_template('404.html'), 404
+@web.errorhandler(404)
+def handle404(e):
+    return render_template('404.html')
+
+
+@web.errorhandler(500)
+def handle500(e):
+    return render_template('500.html')
 
 
 from app.web import book
